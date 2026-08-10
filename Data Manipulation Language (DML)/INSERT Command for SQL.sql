c216b71bd996e9814135d570cc53832cc639340b
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-10 20:34:08 +0530

    Refactor SQL script for INSERT commands to improve readability and organization

--- Data Manipulation Language (DML)/INSERT Command for SQL.sql
+++ Data Manipulation Language (DML)/INSERT Command for SQL.sql
@@ -1,9 +1,12 @@
+
+-- CREATE DATABASE 
 CREATE DATABASE IF NOT EXISTS mydatabase;
 
+-- USE THE DATABASE 
 USE mydatabase;
 
 
-
+-- CREATE THE TABLE users
 CREATE TABLE users(
     uid INTEGER AUTO_INCREMET,
     name VARCHAR(255) NOT NULL,
@@ -12,19 +15,22 @@ CREATE TABLE users(
 );
 
 
+-- INSERT THE SINGLE VALUE :
 INSERT INTO mydatabase.users (uid, name, email, password)
 VALUES('NULL', 'Ayush Suthar', 'ayush@gmail.com', '123123');
 
-
+-- INSERT THE VALUE WITHOUT COLUMNS
 INSERT INTO mydatabase.users
 VALUES('NULL', 'Deepak Chahar', 'deepakchahar@gmail.com', '111123');
 
+-- INSERT THE VALUE WITH SPECIFIC COLUMNS
 INSERT INTO mydatabase.users (name, password)
 VALUES('Ayush Suthar', '123123');
 
+-- INSERT THE VALUES WITH MULTIPLE VALUES AT ONCE
 INSERT INTO mydatabase.users VALUES
 ('NULL', 'Ayushi Chanchalani', 'ayushi@gmail.com', '111111'),
-('NULL', 'Rajat singh', 'rajat@yahoo.com', '121212')
+('NULL', 'Rajat singh', 'rajat@yahoo.com', '121212');
 
 
 
