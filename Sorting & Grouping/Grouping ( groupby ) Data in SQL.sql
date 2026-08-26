9b0f555080629463ae6db156bd432448cbb9367d
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-26 12:37:45 +0530

    Add SQL script for grouping smartphones by various attributes and metrics

---
+++ Sorting & Grouping/Grouping ( groupby ) Data in SQL.sql
@@ -0,0 +1,22 @@
+USE smart;
+
+-- Grouping smart phones as by brand and get the count, avg price, max rating, avg screen size, and battery capacity.
+-- > groupby animation
+
+-- group smartphones by whether they have an NFC and get the avg price and rating.
+
+-- Group smartphones by the extended memory availabe and get the avg price.
+
+-- group smartphones by the brand and processor brand and get the count of models and the avg primary camera resolution ( rear ).
+
+-- find top 5 most costly phones brand.
+
+-- which brand makes the smallest screen smartphones.
+
+-- avg price of 5g phones vs price of non 5g phones
+
+-- group smartphones by the brand and find brand with highest number of models that have both NFC and IR bluster.
+
+-- find all smasung  5g enabled smartphones and find out the  avg price for nfc amd non NFC phones.
+
+-- find the phones namesm, priec of the costliest phone.
