4dd2b2f05fe03841cacd584f65bf9a0841a0c3c0
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-26 12:09:56 +0530

    Add SQL scripts for sorting and grouping data in smartphones

---
+++ Sorting & Grouping/Sorting Data in SQL.sql
@@ -0,0 +1,38 @@
+USE smart;
+
+
+-- find top5 samsung phones with biggest screen size ?
+SELECT model,screen_size FROM smartphones_cleaned_v6 WHERE brand_name = 'samsung'
+ORDER BY screen_size DESC LIMIT 5;
+
+
+-- Sort all the phones in desc order of number of total cameras
+SELECT model, num_front_cameras + num_rear_cameras AS 'total_cameras' 
+FROM smartphones_cleaned_v6
+ORDER BY total_cameras DESC;
+
+--Sort data on the basis of ppi in desc order.
+SELECT model,
+ROUND(SQRT(resolution_height*resolution_height + resolution_width*resolution_width))/screen_size AS "ppi"
+FROM smartphones_cleaned_v6
+ORDER BY ppi DESC;
+
+
+--Find the phone with sec largest battery.
+SELECT model, battery_capacity 
+FROM smartphones_cleaned_v6
+ORDER BY battery_capacity DESC LIMIT 1,1;  
+
+
+--find the name and rating of the worst rated apple phone
+SELECT model, rating FROM smartphones_cleaned_v6 
+WHERE brand_name = 'apple'
+ORDER BY rating DESC LIMIT 3;
+
+--Sort phones alphabethically and then on the basis of rating in desc order.
+SELECT * FROM smartphones_cleaned_v6
+ORDER BY model DESC;
+
+--sort phones alphabethically and then on the basis of price in AESC order
+SELECT * FROM smartphones_cleaned_v6
+ORDER BY model ASC;
