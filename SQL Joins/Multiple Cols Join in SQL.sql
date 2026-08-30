f510fd3f5ccc0508a76a9606bd663656b359f12a
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-30 19:32:47 +0530

    Add SQL queries for additional practice questions on order details and categories

--- SQL Joins/Multiple Cols Join in SQL.sql
+++ SQL Joins/Multiple Cols Join in SQL.sql
@@ -43,5 +43,20 @@ ON t1.user_id = t2.user_id;
 -- 2. find order_id, product category by joining order_details and catergory.
 SELECT t1.order_id, t2.category
 FROM  order_details t1
-JOIN category t2
+JOIN category t2 
 ON t1.category_id = t2.category_id;
+
+-- 3. find all the orders placed in pune.
+SELECT *
+FROM users_1 t1 
+JOIN orders t2 
+ON t1.user_id = t2.user_id
+WHERE t2.city = 'Pune';
+
+-- 4. find all orders under chairs category
+SELECT *
+FROM  order_details t1
+JOIN category t2 
+ON t1.category_id = t2.category_id
+WHERE vertical = 'Chairs';
+
