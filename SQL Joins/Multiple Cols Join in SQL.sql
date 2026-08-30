4da8deba767b3abfc54d666a621f69999ce622d0
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-30 19:15:21 +0530

    Add SQL script demonstrating multiple column joins and practice questions

--- SQL Joins/Multiple Cols Join in SQL.sql
+++ SQL Joins/Multiple Cols Join in SQL.sql
@@ -1,4 +1,47 @@
 USE practice;
 
 
-SELECT * FROM st
+
+-- two columns join :
+SELECT * FROM students t1
+JOIN class t2
+ON t1.class_id = t2.class_id
+AND t1.enrollment_year = t2.class_year;
+
+
+
+SELECT * FROM students t1
+LEFT JOIN class t2
+ON t1.class_id = t2.class_id
+AND t1.enrollment_year = t2.class_year;
+
+
+SELECT * FROM students t1
+RIGHT JOIN class t2
+ON t1.class_id = t2.class_id
+AND t1.enrollment_year = t2.class_year;
+
+
+
+-- Three cols join :
+SELECT * FROM order_details t1
+JOIN orders t2 
+ON t1.order_id = t2.order_id
+JOIN users1 t3
+ON t2.user_id = t3.user_id;
+
+
+
+-- practice questions :
+
+-- 1. find the order_id , name  by joining users and orders table.
+SELECT t2.order_id, t1.name, t1.city 
+FROM users_1 t1 
+JOIN orders t2 
+ON t1.user_id = t2.user_id;
+
+-- 2. find order_id, product category by joining order_details and catergory.
+SELECT t1.order_id, t2.category
+FROM  order_details t1
+JOIN category t2
+ON t1.category_id = t2.category_id;
