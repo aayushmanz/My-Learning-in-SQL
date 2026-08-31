2b04b0ea5d31d8f0b4b9cd8b8e95f388b649f5ed
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-08-31 13:59:58 +0530

    Fix SQL script for independent subqueries: correct table names and enhance comments

--- Subqueries in SQL/2. Independent Subquery in SQL.sql
+++ Subqueries in SQL/2. Independent Subquery in SQL.sql
@@ -4,20 +4,29 @@ USE subquery;
 -- 1. Find the movie with the highest profit (using ORDER BY)
 SELECT  * FROM movies_cleaned
 WHERE (gross - budget) = 
-(SELECT MAX(gross - budget)  
-FROM movies_cleaned);
+                        (SELECT MAX(gross - budget)  
+                         FROM movies_cleaned);
 
 -- one more way :
-SELECT * FROM movies_cleaning
+SELECT * FROM movies_cleaned
 ORDER BY (gross - budget) DESC LIMIT 1;
 
 -- 2. Find how many movies have a rating > the average rating of all movies
 --    (Find the count of movies above the average rating)
-SELECT COUNT(*) FROM movies_cleaning
+SELECT COUNT(*) FROM movies_cleaned
 WHERE score > (SELECT AVG(score)
-FROM movies_cleaning);
+               FROM movies_cleaned);
 
--- 3. Find the highest rated movie among movies with more than 2000 votes
+-- 3. Find the highest scored movie among movies with more than 2000 votes
+SELECT * FROM movies_cleaned
+WHERE year = 2000 AND score = 
+                              (SELECT MAX(score) FROM movies_cleaning
+                               WHERE year = 2000;)
 
--- 4. Find the highest rated movie among all movies whose number of votes
+
+-- 4. Find the highest score movie among all movies whose number of votes
 --    is greater than the average number of votes
+SELECT * FROM movies_cleaned
+WHERE votes > (SELECT AVG(votes) 
+              FROM movies_cleaned)
+ORDER BY MAX(score) LIMIT 1; 
