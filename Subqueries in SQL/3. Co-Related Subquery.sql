e4c82a9e5f743064fdb658605482e962aec9649a
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-01 14:07:53 +0530

    Add SQL script for co-related subqueries: include queries for movies with ratings above genre average and customer favorite foods

---
+++ Subqueries in SQL/3. Co-Related Subquery.sql
@@ -0,0 +1,8 @@
+USE subquery;
+
+
+-- 1. Find All movies that have a rating higher than the average rating of movies in the same genre.
+SELECT * FROM movies_cleaned
+
+
+-- 2. Find the favorite food of each customer.
