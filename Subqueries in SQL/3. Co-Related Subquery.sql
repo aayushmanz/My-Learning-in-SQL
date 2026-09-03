31d35b7b12bfc8afcea7d534c365e91e93301702
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-03 19:40:28 +0530

    Add SQL script for co-related subqueries: include query for favorite food of each customer and address inefficiencies in average score calculation

--- Subqueries in SQL/3. Co-Related Subquery.sql
+++ Subqueries in SQL/3. Co-Related Subquery.sql
@@ -7,3 +7,13 @@ WHERE score > (SELECT AVG(score) FROM movies_cleaned m2
                 WHERE m2.genre = m1.genre );
 
 -- 2. Find the favorite food of each customer.
+WITH fav_food AS (SELECT t2.user_id, name, f_name, COUNT(*) AS "frequency" FROM users t1
+                  JOIN orders t2 ON t1.user_id = t2.user_id
+                  JOIN order_details t3 ON t2.order_id = t3.order_id
+                  JOIN food t4 ON t3.f_id = t4.f_id
+                  GROUP BY t2.user_id, t3.f_id
+                  )
+
+ SELECT * FROM fav_food f1
+ WHERE frequency = (SELECT MAX(frequency) FROM fav_food f2 
+                    WHERE f2.user_id = f1.user_id);
---
+++ Subqueries in SQL/4. Usage of Subquery in SELECT.sql
@@ -0,0 +1,7 @@
+-- 1. Calculate the percentage of votes received by each movie compared to the total number of votes.
+
+-- 2. Display movie name, genre, score, and the average score of its genre.
+
+-- Why is this inefficient?
+-- Because the average score for each genre is calculated repeatedly for every movie,
+-- which can lead to unnecessary computations and slower query performance.
