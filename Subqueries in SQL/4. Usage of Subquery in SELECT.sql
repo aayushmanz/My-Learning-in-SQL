5e02b74a3a0742e566b39c6b730ac6d831fe84c4
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-03 20:13:18 +0530

    Refactor SQL script: improve comments for clarity and maintainability in subquery usage

--- Subqueries in SQL/4. Usage of Subquery in SELECT.sql
+++ Subqueries in SQL/4. Usage of Subquery in SELECT.sql
@@ -1,7 +1,18 @@
 -- 1. Calculate the percentage of votes received by each movie compared to the total number of votes.
+SELECT name, 
+(votes/(SELECT SUM(votes) FROM movies_cleaned))*100 
+FROM movies_cleaned;
+
 
 -- 2. Display movie name, genre, score, and the average score of its genre.
-
 -- Why is this inefficient?
 -- Because the average score for each genre is calculated repeatedly for every movie,
 -- which can lead to unnecessary computations and slower query performance.
+SELECT name, genre, score, (SELECT AVG(score) FROM movies_cleaned m2
+                            WHERE m2.genre  = m1.genre)
+FROM movies_cleaned m1;
+
+
+
+
+-- USE subquery for from :
