68a1f4ca8a6ebbc344e5fe2c9e4fff8c5883f486
AYUSH SUTHAR <sutharayush36@gmail.com>
2026-09-03 19:20:01 +0530

    Refactor SQL script for co-related subqueries: add table alias for clarity in movie rating comparison

--- Subqueries in SQL/3. Co-Related Subquery.sql
+++ Subqueries in SQL/3. Co-Related Subquery.sql
@@ -2,7 +2,8 @@ USE subquery;
 
 
 -- 1. Find All movies that have a rating higher than the average rating of movies in the same genre.
-SELECT * FROM movies_cleaned
-
+SELECT * FROM movies_cleaned m1
+WHERE score > (SELECT AVG(score) FROM movies_cleaned m2
+                WHERE m2.genre = m1.genre );
 
 -- 2. Find the favorite food of each customer.
